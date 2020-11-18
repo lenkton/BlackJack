75436e7c2810e01378bcc087572702249cb6b7c2
lenkton <antonov_s@yahoo.com>
2020-11-18 18:37:32 +0300

    style fixes

--- core/deck.rb
+++ core/deck.rb
@@ -1,10 +1,4 @@
 class Deck < SetOfCards
-  def initialize(init_deck = [])
-    raise BJException, 'init_deck must be an array' unless init_deck.is_a?(Array)
-
-    @cards = init_deck
-  end
-
   def get_card
     @cards.pop
   end
