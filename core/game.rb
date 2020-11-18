2d82947a8cc87c734fc5c599e59e4189241dcfc2
lenkton <antonov_s@yahoo.com>
2020-11-18 21:27:32 +0300

    added unreadable ace handling in the GameState class

--- core/game.rb
+++ core/game.rb
@@ -12,19 +12,32 @@ class GameState
   end
 
   DEFAULT_TJQK_VALUE = 10
+  WIN_POINTS = 21
+  ACE_MAX = 11
+  ACE_MIN = 1
+  ACE_DIF = ACE_MAX - ACE_MIN
 
   private
 
   def assess(cards)
-    cards.inject(0) do |res, card|
+    aces = 0
+    total = cards.inject(0) do |res, card|
       case card.name
       when '*' then '???'
       when /[K,T,Q,J]/ then res + DEFAULT_TJQK_VALUE
-      when /A/ then raise 'IMPLEMENT ACE HANDLING!!'
+      when /A/
+        aces += 1
+        res + ACE_MAX
       when /[2..9]/ then res + card.name.to_i
       else raise 'Incorrect Card name'
       end
     end
+
+    total > WIN_POINTS ? true_aces_value(total, aces) : total
+  end
+
+  def true_aces_value(total, aces)
+    [((total - WIN_POINTS + ACE_DIF - 1) / ACE_DIF), aces].min * ACE_DIF
   end
 end
 
