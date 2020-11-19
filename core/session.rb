915a204d0b077b3bb2c798daab3ae09222c953c1
lenkton <antonov_s@yahoo.com>
2020-11-19 03:20:13 +0300

    added game command support for the Sesstion class

--- core/session.rb
+++ core/session.rb
@@ -1,9 +1,18 @@
 require_relative 'game'
 
 class Session
+  attr_reader :player, :dealer
+
   def initialize(name = 'New Player')
     @player = Player.new(name, [], 100)
     @dealer = Player.new('dealer', [], 100)
     @game = Game.new(@player, @dealer)
+    # @game_state = @game.state
+  end
+
+  (Game.instance_methods - Session.instance_methods).each do |meth|
+    define_method(meth) do # possible bugs with the methods which take arguments
+      @game.send meth
+    end
   end
 end
